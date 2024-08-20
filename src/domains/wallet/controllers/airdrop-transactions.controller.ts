2a8084de867be430d3af3f37f0bc43b3b25f137c
Ajit jain <ajit.jain@freecharge.com>
2024-08-20 23:10:59 +0300

    throttling skipped for airdrop-transactions api

--- src/domains/wallet/controllers/airdrop-transactions.controller.ts
+++ src/domains/wallet/controllers/airdrop-transactions.controller.ts
@@ -2,12 +2,15 @@ import { Controller, Get, Inject } from '@nestjs/common';
 import { WALLET_SERVICE_TOKEN } from '../constants/wallet.constants';
 import { WalletServiceInterface } from '../services/wallet.service.interface';
 import { GetAirdropTransactionsResponse } from '../dtos/get-airdrop-transactions.response.dto';
+import { SkipThrottle } from '@nestjs/throttler';
 
 @Controller('airdrop-transactions')
 export class AirdropTransactionsController {
   constructor(
     @Inject(WALLET_SERVICE_TOKEN) private walletService: WalletServiceInterface,
   ) {}
+
+  @SkipThrottle()
   @Get()
   async getAirdropTransactions(): Promise<GetAirdropTransactionsResponse[]> {
     const transactions = await this.walletService.getAirdropTransactions();
